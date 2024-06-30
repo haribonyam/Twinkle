09124a2c412ac10588171eeefd9049f5c323e9cc
haribonyam <drppr99@gmail.com>
2024-06-30 18:05:20 +0900

    feat: exception handler,customException

--- Twinkle-Backend/src/main/java/com/example/twinkle/common/exception/ErrorCode.java
+++ Twinkle-Backend/src/main/java/com/example/twinkle/common/exception/ErrorCode.java
@@ -1,4 +1,25 @@
 package com.example.twinkle.common.exception;
 
-public class ErrorCode {
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+
+    USER_DUPLICATED_ID(CONFLICT,"이미 가입된 아이디 입니다.","004"),
+    USER_DUPLICATED_NICKNAME(CONFLICT,"이미 가입된 닉네임 입니다.","005"),
+    USER_DUPLICATED_EMAIL(CONFLICT,"이미 가입된 이메일 입니다.","006");
+
+    private final HttpStatus httpStatus;
+    private final String detail;
+    private final String errorCode;
+
+    public static CustomException throwUserDuplicatedId() {
+        throw new CustomException(USER_DUPLICATED_ID);
+    }
 }
